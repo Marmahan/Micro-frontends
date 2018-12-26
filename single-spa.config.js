2185b62148b9687be9a6e6ee53afeb49ebf685a0
marmahan <abdidr@yandex.com>
2018-12-26 12:25:24 +0100

    First commit

---
+++ single-spa.config.js
@@ -0,0 +1,8 @@
+// root-application/single-spa.config.js
+
+import {registerApplication, start} from 'single-spa';
+
+registerApplication('navBar', () => import ('./src/navBar/navBar.app.js').then( module => module.navBar), () => true);
+registerApplication('home', () => import('./src/home/home.app.js'), () => location.pathname === "" || location.pathname === "/" || location.pathname.startsWith('/home'));
+
+start();
