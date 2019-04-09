21659d5a9ecd8bbc088bf4b1b88c4447369b354a
Marmahan <34093500+Marmahan@users.noreply.github.com>
2019-04-09 19:57:15 +0100

    Add files via upload

--- src/signout/root.component.js
+++ src/signout/root.component.js
@@ -11,12 +11,13 @@ import {navigateToUrl} from 'single-spa'
 
 class Signout extends React.Component{
   
-  //once this app is active it expires the cookies and go to the home page
+  //once this app is active, it expires the cookies and go to the home page
   componentDidMount (){
-    location.reload('');//refresh the page
+    //location.reload('');//refresh the page
     document.cookie = 'email' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;'; //set both cookies to an expired date
     document.cookie = 'jwt' + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';//then browser will delete them
     navigateToUrl('/');//go to the home page
+    location.reload('');//refresh the page
 
   }
 
